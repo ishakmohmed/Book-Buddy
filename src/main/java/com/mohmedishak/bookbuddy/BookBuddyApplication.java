11b4eb791531316d9d84082729dd6d473effd7cf
Mohmed Ishak <ishakmohmed1911@gmail.com>
2021-08-26 02:08:15 +0800

    Last commit of the day.

--- src/main/java/com/mohmedishak/bookbuddy/BookBuddyApplication.java
+++ src/main/java/com/mohmedishak/bookbuddy/BookBuddyApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookBuddyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BookBuddyApplication.class, args);
 	}
-
 }
