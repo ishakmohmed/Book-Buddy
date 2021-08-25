c413625bb842aa556ab7c6e88b015aae62956c26
Mohmed Ishak <ishakmohmed1911@gmail.com>
2021-08-26 03:02:08 +0800

    Added a bunch of stuffs.

---
+++ src/main/java/com/bookbuddy/repository/AuthorRepository.java
@@ -0,0 +1,13 @@
+package com.bookbuddy.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bookbuddy.domain.Author;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+	Author findByGivenNameContainingIgnoreCaseAndLastNameContainingIgnoreCase(String givenName, String lastName);
+}
---
+++ src/main/java/com/bookbuddy/repository/BookRepository.java
@@ -0,0 +1,19 @@
+package com.bookbuddy.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bookbuddy.domain.Author;
+import com.bookbuddy.domain.Book;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long>{
+
+	Book findByTitleContainingIgnoreCase(String bookTitle);
+	
+	List<Book> findByGenreContainingIgnoreCase(String genre);
+	
+	List<Book> findByAuthor(Author author);
+}
