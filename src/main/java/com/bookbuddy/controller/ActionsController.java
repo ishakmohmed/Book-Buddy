6f1dcd068955eb600860e6820d3681050bbfaa6e
Mohmed Ishak <ishakmohmed1911@gmail.com>
2021-08-26 20:05:53 +0800

    Added AuthorUtil class which basically has a bunch of messages that will be displayed when the assistand finds author(s) or book(s).

--- src/main/java/com/bookbuddy/controller/ActionsController.java
+++ src/main/java/com/bookbuddy/controller/ActionsController.java
@@ -7,17 +7,22 @@ import javax.servlet.http.HttpServletResponse;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.bookbuddy.service.ActionService;
 
 @RestController
 @RequestMapping("/api/actions/")
 public class ActionsController {
 	private Logger logger = LoggerFactory.getLogger(ActionsController.class);
+	
+	@Autowired
+	private ActionService actionService;
 	
 	@PostMapping
 	public ResponseEntity<?> executePostAction(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -26,6 +31,13 @@ public class ActionsController {
 		String body = request.getReader().lines().collect(Collectors.joining());
 		
 		logger.info(new JSONObject(body).toString(4));
+		
+		try {
+			String intentName = actionService.getIntentName(body);
+		} catch (Exception e) {
+			logger.error("Error: " + e.getMessage());		
+			return new ResponseEntity<String>("Could not process the request", HttpStatus.OK);
+		}
 		
 		return new ResponseEntity<Void>(HttpStatus.OK);
 	}
---
+++ src/main/java/com/bookbuddy/service/ActionService.java
@@ -0,0 +1,41 @@
+package com.bookbuddy.service;
+
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+//import com.bookbuddy.util.IntentUtil;
+import com.google.actions.api.ActionRequest;
+import com.google.actions.api.ActionResponse;
+import com.google.actions.api.DialogflowApp;
+import com.google.actions.api.ForIntent;
+import com.google.actions.api.response.ResponseBuilder;
+
+@Service
+public class ActionService extends DialogflowApp {
+	private Logger logger = LoggerFactory.getLogger(ActionService.class);
+
+//	@ForIntent(IntentUtil.LIST_ACTIONS)
+//	public ActionResponse listActions(ActionRequest request) {
+//		logger.info("Executing intent - " + IntentUtil.LIST_ACTIONS);
+//
+//		StringBuilder response = new StringBuilder();
+//		response.append("I can tell you what authors I know about or you can tell me which"
+//				+ " type of book you are interested in.");
+//		response.append("Based on your selection, we can go forward with the process.");
+//		response.append("So, please tell me, what would you like to know?");
+//
+//		// build the response and send it back
+//		ResponseBuilder responseBuilder = getResponseBuilder(request).add(response.toString());
+//		ActionResponse actionResponse = responseBuilder.build();
+//		return actionResponse;
+//	}
+
+	public String getIntentName(String body) throws Exception {
+		JSONObject bodyJsonObject = new JSONObject(body);
+		JSONObject queryResultObject = bodyJsonObject.getJSONObject("queryResult");
+		JSONObject intentObject = queryResultObject.getJSONObject("intent");
+
+		return intentObject.get("displayName").toString();
+	}
+}
